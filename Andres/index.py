ca2a502ee927da4cab07c2bafdb1bc263ae39397
Andres <andressocorro1@gmail.com>
2021-01-19 19:45:05 -0600

    stock ticker info

--- Andres/index.py
+++ Andres/index.py
@@ -104,6 +104,56 @@ layout1 = html.Div([
 
 app.layout= layout1
 
+#Operators
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
+@app.callback(Output('cards', 'children'),
+[Input('ticker-input', 'value')])
+def refresh_cards(ticker):
+        ticker = ticker.upper()
+        if ticker is None:
+                TICKER = 'Enter a ticker'
+        else:
+                TICKER = yf.Ticker(ticker)
+        
+        cards = [ dbc.Col(make_card("Previous Close ", "secondary", TICKER.info['previousClose']))
+                        , dbc.Col(make_card("Open", "secondary", TICKER.info['open']))
+                        , dbc.Col(make_card("Sector", 'secondary', TICKER.info['sector']))
+                        , dbc.Col(make_card("Beta", 'secondary', TICKER.info['beta']))
+                        , dbc.Col(make_card("50d Avg Price", 'secondary', TICKER.info['fiftyDayAverage']))
+                        , dbc.Col(make_card("Avg 10d Vol", 'secondary', TICKER.info['averageVolume10days']))
+                        ] #end cards list
+        return cards 
+
+
 
 if __name__ == '__main__':
     app.run_server(debug = True)
