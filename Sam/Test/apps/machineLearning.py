0e397c331a4a5f15246748ea2105b40fcf3e57d4
samfon24 <samfons24@yahoo.com>
2021-01-24 21:00:36 -0600

    updated Sam files

--- Sam/Test/apps/machineLearning.py
+++ Sam/Test/apps/machineLearning.py
@@ -26,35 +26,40 @@ import time
 
 
 # This will create the card for ticker
-def make_card(alert_message, color, cardbody, style_dict={"textAlign": "center"}):
-    return dbc.Card([ dbc.Alert(alert_message, color = color), dbc.CardBody(cardbody)], style = style_dict)
+def make_card(alert_message, color, style_dict={"textAlign": "center"}):
+    return dbc.Card([ dbc.Alert(alert_message, color = color)], style = style_dict)
 
-# Takes the input
-def ticker_inputs(inputID):
-
-    return html.Div([
-        dcc.Input(id = inputID, type="text", placeholder="MSFT", style={"textAlign": "center"}),
-        html.P(" "),
-    ])
+# children=html.Div(id='loading-output'
 
 layout = html.Div([
+    html.Br(),
     html.H1('Train your Model!', style={"textAlign": "center"}),
     html.Br(),
+    dbc.Row([dbc.Col(make_card("Search for a stock to train", "primary"))]),
     html.Br(),
-    dbc.Row([dbc.Col(make_card("Search for a stock to train", "primary", ticker_inputs('ticker-input')))]),
+    html.Div([dcc.Input(id='input-box', placeholder='MSFT', type='text', style={"textAlign": "center"}),
     html.Br(),
-    html.Div(dcc.Input(id='input-box', placeholder='MSFT', type='text', style={"textAlign": "center"}), style={"textAlign": "center"}),
+    html.Br(),
     html.Div(html.Button('Train Model', id='button'), style={"textAlign": "center"}),
+    html.Br(),
+    dcc.Loading(id='loading-output', type='graph', fullscreen=True ,style={"textAlign": "center"})], style={"textAlign": "center"}),
     html.Div(id='output-container-button', children='Enter stock ticker and click submit', style={"textAlign": "center"}),
+    html.Br(),
     html.Div(id='model-graph', style={"textAlign": "center"}),
     html.Br(),
-    html.Div(id='model-summary-0', style={"textAlign": "center", 'whiteSpace': 'pre-wrap'}),
+    html.Div(html.H3(id='model-summary-title', style={"textAlign": "center"})),
     html.Br(),
-    html.Div(id='model-summary-1', style={"textAlign": "center", 'whiteSpace': 'pre-wrap'})
+    html.Div(id='model-summary-0', style={"textAlign": "center"}),
+    html.Br(),
+    html.Div(id='model-summary-1', style={"textAlign": "center", 'whiteSpace': 'pre-wrap'}),
 ])
 
 @app.callback(
-    [Output('model-graph', 'children'), Output('model-summary-0', 'children'), Output('model-summary-1', 'children')],
+    [Output('model-graph', 'children'), 
+        Output('model-summary-0', 'children'), 
+        Output('model-summary-1', 'children'), 
+        Output('loading-output', 'children'), 
+        Output('model-summary-title', 'children')],
     [Input('button', 'n_clicks')],
     [State('input-box', 'value')])
 def update_output(n_clicks, value):
@@ -114,63 +119,14 @@ def update_output(n_clicks, value):
             html = model_fit.summary().tables[i].as_html()
             df_table = pd.read_html(html, header=0, index_col=0)[0]
             df_table = df_table.reset_index()
+            # df_table = df_table.dropna
             table1 = dbc.Table.from_dataframe(df_table, striped=True, bordered=True, hover=True)
         else:
             html = model_fit.summary().tables[i].as_html()
             df_table = pd.read_html(html, header=0, index_col=0)[0]
             df_table = df_table.reset_index()
             table2 = dbc.Table.from_dataframe(df_table, striped=True, bordered=True, hover=True)
-
-    return graph, table1, table2
-
-# @app.callback([Output('model-summary-0', 'children'), Output('model-summary-1', 'children')],
-#     [Input('button', 'n_clicks')],
-#     [State('input-box', 'value')])
-# def update_output(n_clicks, value):
-#     ticker = value.upper()
-#     df = yf.Ticker(ticker).history(period='Max')
-#     # df.reset_index(inplace=True)
-#     df = df.filter(['Close'])
-
-#     # Define the p, d and q parameters to take any value between 0 and 3
-#     p = d = q = range(0, 3)
-#     # Generate all different combinations of p, q and q
-#     pdq = list(itertools.product(p, d, q))
-
-#     warnings.filterwarnings("ignore")
-#     aic= []
-#     parameters = []
-#     for param in pdq:
-#     #for param in pdq:
-#         try:
-#             mod = sm.tsa.statespace.SARIMAX(df, order=param, enforce_stationarity=True, enforce_invertibility=True)
-#             results = mod.fit()
-#             # save results in lists
-#             aic.append(results.aic)
-#             parameters.append(param)
-#             #seasonal_param.append(param_seasonal)
-#             # print('ARIMA{} - AIC:{}'.format(param, results.aic))
-#         except:
-#             continue
-#     # find lowest aic          
-#     index_min = min(range(len(aic)), key=aic.__getitem__)           
-
-#     print('The optimal model is: ARIMA{} -AIC{}'.format(parameters[index_min], aic[index_min]))
-
-#     model = ARIMA(df, order=parameters[index_min])
-#     model_fit = model.fit(disp=0)
-
-#     # Print Summary for Website
-#     for i in range(2):
-#         if i == 0:
-#             html = model_fit.summary().tables[i].as_html()
-#             df_table = pd.read_html(html, header=0, index_col=0)[0]
-#             df_table = df_table.reset_index()
-#             table1 = dbc.Table.from_dataframe(df_table, striped=True, bordered=True, hover=True)
-#         else:
-#             html = model_fit.summary().tables[i].as_html()
-#             df_table = pd.read_html(html, header=0, index_col=0)[0]
-#             df_table = df_table.reset_index()
-#             table2 = dbc.Table.from_dataframe(df_table, striped=True, bordered=True, hover=True)
-
-#     return table1, table2
+    
+    empty_string = ''
+    table_title = 'Arima Summary Results'
+    return graph, table1, table2, empty_string, table_title
